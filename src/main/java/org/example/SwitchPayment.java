1e405f587293a35f15ebb605068987faf690e028
gulzat1003 <gulzat.aldashova.ch@gmail.com>
2023-01-26 23:37:47 +0600

    ChangeSwiftPayment

--- src/main/java/org/example/SwitchPayment.java
+++ src/main/java/org/example/SwitchPayment.java
@@ -1,7 +1,8 @@
 package org.example;
 
-public class SwiftPayment {
+public class SwitchPayment {
     public static void main(String[] args) {
         System.out.println("This is SWIFT payment");
+        System.out.println("This is a test");
     }
 }
