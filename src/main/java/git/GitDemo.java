2ccf86a572b4d79bd9054553fe127c7ab4958bc3
Gulzat <gulzat.aldashova.ch@gmail.com>
2023-01-28 18:09:13 +0600

    I creat new Git Class

--- src/main/java/git/GitDemo.java
+++ src/main/java/git/GitDemo.java
@@ -2,6 +2,6 @@ package git;
 
 public class GitDemo {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello World!");
     }
 }
