5e9a26b8724692a76161c76a43c418c019e4515a
gulzat1003 <123569776+gulzat1003@users.noreply.github.com>
2023-01-28 16:28:38 +0600

    Delete SwitchPayment.java

--- src/main/java/org/example/SwitchPayment.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class SwitchPayment {
-    public static void main(String[] args) {
-        System.out.println("This is SWIFT payment");
-        System.out.println("This is a test");
-    }
-}
